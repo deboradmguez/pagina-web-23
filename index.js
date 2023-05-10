3b5740ce01c8d92038c5c7eeb962b3c90b2b0a1a
Debora <debodominguez58@gmail.com>
2023-05-10 03:49:04 -0300

    done.. i think

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ btnSubirArriba.addEventListener('click', (e) => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
+// calculo descuento
+function getValueFromInput() {
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var correo = document.getElementById("correo").value;
+    var cantidad = document.getElementById("inputCantidad").value;
+    var descuento = document.getElementById("inputDescuento").value;
+
+    // descuento = parseFloat(descuento);
+
+    // Calcular el descuento
+    var descuentoAplicado = cantidad * 200 * descuento;
+
+    // Restar el descuento al total
+    var total = cantidad * 200 - descuentoAplicado;
+
+    document.getElementById("a-pagar").textContent = total.toFixed(2);
+}
