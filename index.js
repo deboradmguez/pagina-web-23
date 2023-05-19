64c90a34be58c92d47be3e9a544c73cd80ca9802
Debora <debodominguez58@gmail.com>
2023-05-19 17:18:45 -0300

    cambios

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-// funcion boton subir
-const btnSubirArriba = document.querySelector('.btn-subir');
-
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 0) {
-        btnSubirArriba.classList.remove('d-none');
-    } else {
-        btnSubirArriba.classList.add('d-none');
-    }
-});
-
-btnSubirArriba.addEventListener('click', (clickEvent) => {
-    clickEvent.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-
 // calculo descuento
 function getValueFromInput() {
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var correo = document.getElementById("correo").value;
-    var cantidad = document.getElementById("inputCantidad").value;
-    var descuento = document.getElementById("inputDescuento").value;
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let correo = document.getElementById("correo").value;
+    let cantidad = document.getElementById("inputCantidad").value;
+    let descuento = document.getElementById("inputDescuento").value;
 
-    // descuento = parseFloat(descuento);
+    // calcular el descuento
+    let descuentoAplicado = cantidad * 200 * descuento;
 
-    // Calcular el descuento
-    var descuentoAplicado = cantidad * 200 * descuento;
-
-    // Restar el descuento al total
-    var total = cantidad * 200 - descuentoAplicado;
+    // restar el descuento al total
+    let total = cantidad * 200 - descuentoAplicado;
 
     document.getElementById("a-pagar").textContent = total.toFixed(2);
 }
